5059af8babeb4ca73e75c076421dd0894762cc06
Josh Smith <jsmith@dailyprinting.local>
2023-12-28 15:05:27 -0600

    Updated Converter

--- src/components/Converter.tsx
+++ src/components/Converter.tsx
@@ -1,52 +1,59 @@
 import { useState } from 'react'
 
 function Converter() {
-    //Variables that will change on change
+    //****Variables that will change on change
     const [unitType, setUnitType] = useState([""]);
+    const [convertFrom, setConvertFrom] = useState("");
+    const [convertTo, setConvertTo] = useState("");
 
-
-    //List of unit types
+    //****List of unit types
     const lengthtypes = ["Centimeter", "Inch", "Feet"]
-    const distanttypes = ["Meter", "Kilometer", "Mile", "Foot", "Inch"]
     const temptypes = ["Celsius", "Kelvin", "Fahrenheit"]
-    const timetypes = ["Second", "Minute", "Hour", "Day", "Week", "Month", "Year"]
-    const weighttypes = ["Kilogram", "Gram", "Metric Ton", "Pound", "Ounce"]
-    const volumetypes = ["US Gallon", "US Quart", "US Pint", "US Cup", "Imperial Gallon", "Imperial Quart", "Imperial Pint", "Imperial Cup"]
+    // const distanttypes = ["Meter", "Kilometer", "Mile", "Foot", "Inch"]
+    // const timetypes = ["Second", "Minute", "Hour", "Day", "Week", "Month", "Year"]
+    // const weighttypes = ["Kilogram", "Gram", "Metric Ton", "Pound", "Ounce"]
+    // const volumetypes = ["US Gallon", "US Quart", "US Pint", "US Cup", "Imperial Gallon", "Imperial Quart", "Imperial Pint", "Imperial Cup"]
 
-    const allUnitTypes = [lengthtypes, distanttypes, temptypes, timetypes, weighttypes, volumetypes]
+    const allUnitTypes = [temptypes, lengthtypes]
 
 
-    //On Change function that detects when a new measurement type is selected
+    //****On Change function that detects when a new measurement type is selected
     const MeasurementChanged = (e:any) => {
         let index = parseInt(e.target.value)
         setUnitType(allUnitTypes[index])
     }
+
+    //*************************** CONVERSIONS ***************************/
+    let display = document.querySelector("#display")
+    let secondInput = document.querySelector("#unit_input_2")
+
+    const newValueDetected = (e:any) => {
+        let valueToConvert = e.target.value;
+    }
+
+    //**************************************************************** */
 
 
     return (
         <div className="Converter Center Column">
             <select id="Measurements" onChange={MeasurementChanged}>
                 <option value="" disabled selected>Measurement Type</option>
-                <option value="0">Length</option>
-                <option value="1">Distance</option>
-                <option value="2">Temperature</option>
-                <option value="3">Time</option>
-                <option value="4">Weight</option>
-                <option value="5">Volume</option>
+                <option value="0">Temperature</option>
+                <option value="1">Length</option>
             </select>
             <div className="Inputs Center">
                 <div className="Input_Container Center Row">
-                    <select id="unit_picker_1">
+                    <select id="unit_picker_1" onChange={(e) => {setConvertFrom(e.target.value)}}>
                         <option value="" disabled selected>Unit</option>
                         {unitType.map(option => (
                             <option value={option}>{option}</option>
                         ))}
                     </select>
-                    <input id="unit_input_1" type="number" />
+                    <input id="unit_input_1" type="number" onChange={newValueDetected}/>
                 </div>
                 <button id="switch">Switch</button>
                 <div className="Input_Container Center Row">
-                    <select id="unit_picker_2">
+                    <select id="unit_picker_2" onChange={(e) => {setConvertTo(e.target.value)}}>
                         <option value="" disabled selected>Unit</option>
                         {unitType.map(option => (
                             <option value={option}>{option}</option>
